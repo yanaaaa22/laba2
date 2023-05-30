b11c06d702ef4263156b4d554288f468b998b7e8
yanaaaa22 <125030360+yanaaaa22@users.noreply.github.com>
2023-05-30 17:09:49 +0300

    Delete lab_6_1.py

--- lab_6_1.py
+++
@@ -1,7 +0,0 @@
-country = {'Россия': 'Москва', 'Казахстан':'Астана', 'Индия':'Нью Дели', 'Тайланд':'Бангкок'}
-list_country = list(country.keys())
-list_country.sort()
-for i in list_country:
-    print(i, ':', country[i])
-print(country)
-print(country['Казахстан'])
